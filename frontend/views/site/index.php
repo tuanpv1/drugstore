8480c47ebf4e14726b1f2c6f8817b3441f1ffc41
tuanpv1 <tuantrylook@gmail.com>
2017-03-29 13:16:35 +0700

    a

--- frontend/views/site/index.php
+++ frontend/views/site/index.php
@@ -48,8 +48,8 @@ use yii\helpers\Url;
                                 <a class="button border-radius background-primary text-size-12 text-white text-strong"
                                    href="javascript:void(0)" onclick="addCart(<?= $item->id ?>)">Mua</a>
                             </div>
-                            <p><?= $item->short_des?$item->short_des:'Đang cập nhật'?></p>
                         </div>
+                        <p><?= $item->short_des?$item->short_des:'Đang cập nhật'?></p>
                     </div>
                     <?php
                 }
