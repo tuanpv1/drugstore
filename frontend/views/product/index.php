8a31abace19ad1689bedc44586cacf9224abb6b6
tuanpv1 <tuantrylook@gmail.com>
2017-03-29 13:35:42 +0700

    a

--- frontend/views/product/index.php
+++ frontend/views/product/index.php
@@ -16,21 +16,19 @@ use yii\helpers\Url;
                             ?>
                             <div class="s-12 m-12 l-4 margin-bottom">
                                 <div class="padding-2x block-bordered border-radius">
-                                    <div class="line">
-                                        <div class="s-12 m-12 l-6 margin-bottom-30">
-                                            <a class="image-hover-zoom" href="<?= Url::to(['product/detail','id'=>$item->id]) ?>"><img style="height:135px" src="<?= $item->getImageLink() ?>" alt="<?= $item->name ?>"></a>
-                                        </div>
-                                        <div  class="s-12 m-12 l-6">
-                                            <h4 class="text-thin"><?= Product::substr($item->name,42) ?></h4>
-                                            <p class="margin-bottom-30">Giá: <?= Product::formatNumber($item->price) ?> VND</p>
-
-                                        </div>
+                                    <div class="s-12 m-12 l-6 text-center">
+                                        <a class="image-hover-zoom" href="<?= Url::to(['product/detail','id'=>$item->id]) ?>"><img style="height:135px" src="<?= $item->getImageLink() ?>" alt="<?= $item->name ?>"></a>
                                     </div>
-                                    <div class="s-12 m-12 l-12 center">
+                                    <a style="margin-top: 25px;color: #00aa00" href="<?= Url::to(['product/detail','id'=>$item->id]) ?>"><?= Product::substr($item->name,42) ?></a>
+                                    <p class="margin-bottom-30">Giá: <?= Product::formatNumber($item->price) ?> VND</p>
+                                    <div>
                                         <a class="button border-radius background-success text-size-12 text-white text-strong"
-                                           href="<?= Url::to(['product/detail','id'=>$item->id]) ?>">Xem chi tiết</a>
+                                           href="<?= Url::to(['product/detail','id'=>$item->id]) ?>">Xem </a>
                                         <a class="button border-radius background-primary text-size-12 text-white text-strong"
                                            href="javascript:void(0)" onclick="addCart(<?= $item->id ?>)">Mua</a>
+                                    </div>
+                                    <div class="1-12 text-center"><br>
+                                        <p><?= $item->short_des?$item->short_des:'Đang cập nhật'?></p>
                                     </div>
                                 </div>
                             </div>
