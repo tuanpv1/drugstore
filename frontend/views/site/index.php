7a2aa3ab990fe51ef4909eeb2079434adf5190bd
tuanpv1 <tuantrylook@gmail.com>
2017-03-29 20:21:26 +0700

    s

--- frontend/views/site/index.php
+++ frontend/views/site/index.php
@@ -74,7 +74,7 @@ use yii\helpers\Url;
         <div class="carousel-default owl-carousel carousel-wide-arrows">
             <div class="item">
                 <div class="margin">
-                <?php $index = 0; foreach ($post as $value) { ?>
+                <?php $index = 1; foreach ($post as $value) { ?>
                     <div class="s-12 m-12 l-6">
                         <div class="image-border-radius margin-m-bottom">
                             <div class="margin">
@@ -95,13 +95,13 @@ use yii\helpers\Url;
                         </div>
                     </div>
                 <?php
-                $index++;
-                if ($index % 2 == 0 && $index <= count($post) && $index !=0 ) { ?>
+                if ($index % 2 == 0 && $index <= count($post) && $index !=0 ) {?>
                 </div>
             </div>
             <?php
             }
-        }
+            $index++;
+            }
             ?>
         </div>
     </div>
