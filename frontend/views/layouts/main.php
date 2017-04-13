60a9d2a5cee158feb0d25a6fc63f4acc18a5481f
tuanpv1 <tuantrylook@gmail.com>
2017-04-13 13:16:46 +0700

    thay doi cho

--- frontend/views/layouts/main.php
+++ frontend/views/layouts/main.php
@@ -74,7 +74,15 @@ $title = "Bổ thận tráng dương"
     <?= Alert::widget() ?>
     <?= $content ?>
 </div>
-
+<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+<!-- Quảng cáo trên nhà thuốc -->
+<ins class="adsbygoogle"
+     style="display:inline-block;width:728px;height:90px"
+     data-ad-client="ca-pub-1810353436941461"
+     data-ad-slot="7431247833"></ins>
+<script>
+    (adsbygoogle = window.adsbygoogle || []).push({});
+</script>
 <footer>
     <!-- Social -->
     <div class="background-primary padding text-center">
@@ -185,15 +193,6 @@ $title = "Bổ thận tráng dương"
             </div>
         </div>
     </section>
-    <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
-    <!-- Quảng cáo trên nhà thuốc -->
-    <ins class="adsbygoogle"
-         style="display:inline-block;width:728px;height:90px"
-         data-ad-client="ca-pub-1810353436941461"
-         data-ad-slot="7431247833"></ins>
-    <script>
-        (adsbygoogle = window.adsbygoogle || []).push({});
-    </script>
 </footer>
     <div class="line bottombar">
         <div class="s-12 m-12 l-12">
